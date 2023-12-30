0e2af88bb7595ea5ec31954877105c4e9763b7d0
Porneliusss <11111pyk@mail.ru>
2023-12-30 17:47:44 +0300

    fix: update routes.js

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,16 +1,27 @@
 import Main from "./pages/MainPage";
 import Auth from "./pages/AuthPage";
-import { MAIN_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, PRODUCT_ROUTE, ADMIN_ROUTE, PRODUCT_ITEM_ROUTE, PROFIL_ROUTE, CONSOLIDATION_ROUTER } from "./utils/consts";
+import { MAIN_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, PRODUCT_ROUTE, ADMIN_ROUTE, PRODUCT_ITEM_ROUTE, PROFIL_ROUTE, CONSOLIDATION_ROUTER, PACKAGE_CREATE_ROUTER, WAYBILL_ROUTER, WAYBILL_PAGE_ROUTER} from "./utils/consts";
 import ProductListPage from "./pages/ProductListPage";
 import AdminPage from "./pages/AdminPage";
 import ProductPage from "./pages/ProductPage";
 import Profile from "./pages/Profile";
 import ConsolidationPage from "./pages/ConsolidationPage";
+import CreatePackagePage from "./pages/CreatePackagePage";
+import CarWayBill from "./pages/CarWayBill";
+import Waybill from "./pages/WayBill";
 
 export const authRoutes = [
     {
         path: ADMIN_ROUTE,
         Component: AdminPage
+    },
+    {
+        path: WAYBILL_ROUTER + '/:id',
+        Component:  CarWayBill 
+    },
+    {
+        path: WAYBILL_PAGE_ROUTER,
+        Component:  Waybill 
     },
 ];
 
@@ -18,6 +29,10 @@ export const publicRoutes = [
     {
         path: MAIN_ROUTE,
         Component: Main
+    },
+    {
+        path: PACKAGE_CREATE_ROUTER,
+        Component: CreatePackagePage
     },
     {
         path: LOGIN_ROUTE,
