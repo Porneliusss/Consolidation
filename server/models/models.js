3015c353a1a73d9dedf4eb6f37f62789d935905f
Porneliusss <11111pyk@mail.ru>
2023-12-30 17:22:02 +0300

    fix: fixed models

--- server/models/models.js
+++ server/models/models.js
@@ -8,6 +8,42 @@ const User = sequelize.define('user', {
     role: { type: DataTypes.STRING, defaultValue: "USER" },
 })
 
+const Package = sequelize.define('package', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    startPlace: { type: DataTypes.STRING, allowNull: true },
+    endPlace: { type: DataTypes.STRING, allowNull: true },
+    weight: { type: DataTypes.DECIMAL, allowNull: false },
+    length: { type: DataTypes.DECIMAL, allowNull: false },
+    width: { type: DataTypes.DECIMAL, allowNull: false },
+    status: { type: DataTypes.INTEGER, allowNull: false },
+    // 0 - init
+    // 1 - закреплена машина
+    // 2 - в процессе
+    // 3 - доставлена
+})
+
+const Car = sequelize.define('car', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    startPlace: { type: DataTypes.STRING, allowNull: true },
+    number: { type: DataTypes.INTEGER, allowNull: true },
+    endPlace: { type: DataTypes.STRING, allowNull: true },
+    trunkVolume: { type: DataTypes.DECIMAL, allowNull: false },
+    status: { type: DataTypes.STRING, allowNull: true },
+})
+
+const PackageCar = sequelize.define('packageCar', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    carId: { type: DataTypes.INTEGER, allowNull: false },
+    packageId: { type: DataTypes.INTEGER, allowNull: false }
+})
+
+const Waybill = sequelize.define('waybill', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    date: { type: DataTypes.DATE, allowNull: false },
+    status: { type: DataTypes.STRING, allowNull: true },
+    number: { type: DataTypes.INTEGER, allowNull: true },
+})
+
 const Consolidation = sequelize.define('consolidation', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     endPlace: { type: DataTypes.STRING, allowNull: true },
@@ -63,6 +99,9 @@ Order.belongsTo(User);
 User.hasMany(Advice);
 Advice.belongsTo(User);
 
+User.hasMany(Package);
+Package.belongsTo(User);
+
 Category.hasMany(Product);
 Product.belongsTo(Category);
 
@@ -72,11 +111,22 @@ Product.belongsTo(Provider);
 Product.hasMany(Order);
 Order.belongsTo(Product);
 
+PackageCar.hasMany(Waybill);
+Waybill.belongsTo(PackageCar);
+
+
 Order.belongsToMany(Consolidation, { through: OrderConsolidation });
 Consolidation.belongsToMany(Order, { through: OrderConsolidation });
 
+Car.belongsToMany(Package, { through: PackageCar, as: 'cars' });
+Package.belongsToMany(Car, { through: PackageCar, as: 'packages' });
+
 module.exports = {
     User,
+    Car,
+    PackageCar,
+    Waybill,
+    Package,
     Consolidation,
     Order,
     Product,
